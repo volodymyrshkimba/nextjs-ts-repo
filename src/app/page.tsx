212694a0432e0cbe836b4eeb515ce5d947049d80
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-02-23 05:03:18 +0200

    add form and modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import { headers } from 'next/headers';
+import AddCompanyButton from './components/add-company-button';
 import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
-  console.log(headers());
-
   return (
     <main>
-      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <h1 className="text-xl">Home page</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
       <StatusLabel status={Status.Suspended} disabled>
         Suspended
       </StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }
