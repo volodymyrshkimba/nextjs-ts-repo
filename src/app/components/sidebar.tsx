59e2c02ecdfd9423236b77a63a1cb699248bf555
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-03-02 21:34:11 +0200

    add Link navigation and navigation marker

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,10 +1,20 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import SidebarItem from '@/app/components/sidebar-item';
+import { usePathname, useRouter } from 'next/navigation';
 
 // export interface SidebarProps {}
 
 export default function Sidebar({}: object) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = () => {
+    router.push('/');
+  };
+
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -16,14 +26,24 @@ export default function Sidebar({}: object) {
           alt="logo"
         />
         <ul className="space-y-7">
-          <SidebarItem pathname="/dashboard" src="/icons/squares.svg" alt="dashboard icon">
+          <SidebarItem
+            current={pathname === '/dashboard'}
+            pathname="/dashboard"
+            src="/icons/squares.svg"
+            alt="dashboard icon"
+          >
             Dashboard
           </SidebarItem>
-          <SidebarItem pathname="/companies" src="/icons/briefcase.svg" alt="companies icon">
+          <SidebarItem
+            current={pathname === '/companies'}
+            pathname="/companies"
+            src="/icons/briefcase.svg"
+            alt="companies icon"
+          >
             Companies
           </SidebarItem>
         </ul>
-        <button className="flex items-center gap-2 p-6 mt-auto mx-auto">
+        <button onClick={handleClick} className="flex items-center gap-2 p-6 mt-auto mx-auto">
           <Image width={18} height={18} src="/icons/arrow-left-on-rectangle.svg" alt="exit icon" />
           <span className="font-medium text-white">Exit</span>
         </button>
